9f10bcb48cfa6c8d8af84495f5db9b0fb383b3a3
Juraj Kubala <juraj.alabuk@gmail.com>
2016-11-14 23:04:49 +0200

    remove unnecessary files

--- _src/shared/constants/SocketEvents.js
+++
@@ -1,8 +0,0 @@
-export const INIT_SETUP = 'INIT_SETUP';
-
-export const USER_LOGIN = 'USER_LOGIN';
-export const USER_LOGOUT = 'USER_LOGOUT';
-
-export const CHANNEL_NEW = 'CHANNEL_NEW';
-
-export const MESSAGE_NEW = 'MESSAGE_NEW';
